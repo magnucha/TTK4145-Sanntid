74945b733530bab74f2fa88d0b5a9cc4bcd10c8f
Magnus Harr <magnus.harr@gmail.com>
2016-01-20 09:46:57 +0100

    Finished threading_go.go

--- Øving2/threading_go.go
+++ Øving2/threading_go.go
@@ -9,25 +9,44 @@ import (
     "time"
 )
 
-var i int = 0
-
-func goroutine1() {
+func goroutine1(chan1 chan<- int, chan_finished chan<- int) {
     for j := 0; j < 1000000; j++ {
-        i++
+        chan1 <- 1;
     }
+    chan_finished <- 1
 }
-func goroutine2() {
+func goroutine2(chan1 chan<- int, chan_finished chan<- int) {
     for k := 0; k < 1000000; k++ {
-        i--
+        chan1 <- -1;
     }
+    chan_finished <- 1
+}
+
+func server(chan1 <-chan int, chan_finished <-chan int) int {
+	var i int = 0
+	var threads_finished int = 0
+	for threads_finished < 2 {
+		select {
+			case msg1 := <- chan1:
+				i += msg1
+			case <- chan_finished:
+				threads_finished++
+		}
+	}
+	return i
 }
 
 
 func main() {
     runtime.GOMAXPROCS(runtime.NumCPU())    // I guess this is a hint to what GOMAXPROCS does...
                                             // Try doing the exercise both with and without it!
-    go goroutine1()                      // This spawns someGoroutine() as a goroutine
-    go goroutine2()
+    
+    chan1 := make(chan int)
+    chan2 := make(chan int)
+    
+    go goroutine1(chan1, chan2)                      // This spawns someGoroutine() as a goroutine
+    go goroutine2(chan1, chan2)
+    i := server(chan1, chan2)
 
     // We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
     // We'll come back to using channels in Exercise 2. For now: Sleep.
